b24141fbb97fa622222b414d44fea73ff407ffc0
Fabian Beck <fabian.beck@uni-bamberg.de>
2022-12-15 13:34:40 +0100

    Adding vuetify UI library

--- src/main.js
+++ src/main.js
@@ -4,7 +4,18 @@ import App from './App.vue'
 
 import './assets/main.css'
 
-const app = createApp(App)
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+const app = createApp(App).use(vuetify).mount('#app')
 
 app.use(createPinia())
 
