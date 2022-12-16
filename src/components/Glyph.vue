4d9f3108c2797418d530dc3e83258180ff71415e
Fabian Beck <fabian.beck@uni-bamberg.de>
2022-12-16 14:34:39 +0100

    Glyph as simple pie chart

--- src/components/Glyph.vue
+++ src/components/Glyph.vue
@@ -1,3 +1,31 @@
+<script setup>
+import * as d3 from "d3";
+
+import { onMounted } from "vue";
+
+import { useCategoryStore } from "../stores/category.js";
+
+onMounted(() => {
+  const categoriesStore = useCategoryStore();
+  const data = categoriesStore.categories.map((category) => category.value);
+
+  const pie = d3.pie();
+  const arc = d3.arc().innerRadius(0).outerRadius(50);
+
+  const width = 100;
+  const height = 100;
+  const svg = d3
+    .select("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [-width / 2, -height / 2, width, height])
+    .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+
+  svg.append("g").selectAll("path").data(pie(data)).join("path").attr("d", arc);
+});
+</script>
+
 <template>
-    <v-icon icon="mdi-circle-slice-5" size="x-large"></v-icon>
+  <svg></svg>
+  <p>{{ categories }}</p>
 </template>
