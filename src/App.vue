dc3399146bc89fc9c54258dcc277b0a460c4ffc1
Fabian Beck <fabian.beck@uni-bamberg.de>
2022-12-16 09:43:11 +0100

    Section IDs

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,27 @@ import Personalization from "./components/Personalization.vue";
 
 const sections = [
   {
+    id: "map",
     title: "Map",
     icon: "mdi-map",
   },
   {
+    id: "ratings",
     title: "Ratings",
     icon: "mdi-star",
   },
   {
+    id: "good-and-bad",
     title: "Good and Bad",
     icon: "mdi-plus-minus-variant",
   },
   {
+    id: "stories",
     title: "Stories",
     icon: "mdi-comment-quote",
   },
   {
+    id: "fun",
     title: "Fun Facts",
     icon: "mdi-party-popper",
   },
@@ -56,20 +61,22 @@ const sections = [
         </div>
         <div v-for="section in sections" :key="section.title" class="my-6">
           <div class="text-h5">
-            <v-icon :icon="section.icon" class="mr-2"></v-icon>{{ section.title }}
+            <a :id="section.id"></a>
+            <v-icon :icon="section.icon" class="mr-2"></v-icon
+            >{{ section.title }}
           </div>
           <div class="ml-12">
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae
-              at, ratione odit iure deleniti quae corrupti libero ex adipisci
-              nobis nihil eveniet hic autem aliquid reprehenderit mollitia.
-              Temporibus, repudiandae quae.
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Molestiae at, ratione odit iure deleniti quae corrupti libero ex
+              adipisci nobis nihil eveniet hic autem aliquid reprehenderit
+              mollitia. Temporibus, repudiandae quae.
             </p>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
               harum accusantium earum saepe neque. Earum quo, tempora cupiditate
-              quod repellat praesentium ad dicta odit dolores alias. Eligendi quos
-              veritatis animi?
+              quod repellat praesentium ad dicta odit dolores alias. Eligendi
+              quos veritatis animi?
             </p>
           </div>
         </div>
