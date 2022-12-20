50b51069e078d58c8ec2245a4f23cec6d637c59e
Fabian Beck <fabian.beck@uni-bamberg.de>
2022-12-20 20:22:48 +0100

    Summary sentence on ratings

--- src/components/HotelOverview.vue
+++ src/components/HotelOverview.vue
@@ -1,6 +1,7 @@
 <script>
 import HotelRating from "./HotelRating.vue";
 import { useHotelStore } from "../stores/hotel.js";
+import { useCategoryStore } from "../stores/category.js";
 
 export default {
   components: {
@@ -8,19 +9,49 @@ export default {
   },
   setup() {
     const hotelStore = useHotelStore();
-    return { hotelStore };
+    const categoryStore = useCategoryStore();
+    return { hotelStore, categoryStore };
+  },
+  computed: {
+    ratingVarietyDescription() {
+      const minRatings = this.hotelStore.minRatings;
+      const maxRatings = this.hotelStore.maxRatings;
+      const normalizedCategoryValues =
+        this.categoryStore.normalizedCategoryValues;
+      const variety = Object.keys(minRatings).reduce(
+        (sum, categoryId) =>
+          (sum +=
+            Math.abs(minRatings[categoryId] - maxRatings[categoryId]) *
+            normalizedCategoryValues[categoryId]),
+        0
+      );
+      console.log(variety);
+      return variety > 0.99 ? "somewhat differently" : "quite similarly";
+    },
   },
 };
 </script>
 
 <template>
-  <div class="d-flex flex-column hotel-list ml-12 my-4">
-    <HotelRating
-      v-for="hotel in hotelStore.hotels"
-      :key="hotel.id"
-      :hotel="hotel"
-    ></HotelRating>
-    <v-label class="ml-4">Rating weighted by priorities <v-icon icon="mdi-arrow-right"></v-icon></v-label>
+  <div class="ml-12">
+    <p>
+      Customers have rated the
+      <b
+        ><span>{{ hotelStore.hotels.length }}</span></b
+      >
+      selected hotels <b>{{ ratingVarietyDescription }}</b> with respect to the
+      prioritized categories.
+    </p>
+    <div class="d-flex flex-column hotel-list my-4">
+      <HotelRating
+        v-for="hotel in hotelStore.hotels"
+        :key="hotel.id"
+        :hotel="hotel"
+      ></HotelRating>
+      <v-label class="ml-4"
+        >Rating weighted by priorities <v-icon icon="mdi-arrow-right"></v-icon
+      ></v-label>
+    </div>
   </div>
 </template>
 
--- src/stores/hotel.js
+++ src/stores/hotel.js
@@ -86,7 +86,6 @@ export const useHotelStore = defineStore({
           ),
           0
         )
-
     },
   },
 })
