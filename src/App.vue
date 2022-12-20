3f88496c744b999df99349146008276352292115
Fabian Beck <fabian.beck@uni-bamberg.de>
2022-12-20 17:20:08 +0100

    Banner image

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,6 @@ const sections = [
 ];
 </script>
 
-
 <template>
   <v-app>
     <v-app-bar title="Hotel Review Comparison"></v-app-bar>
@@ -55,8 +54,12 @@ const sections = [
     /></v-navigation-drawer>
     <v-main class="ma-6">
       <div class="content mx-auto">
+        <div class="text-h1 my-16">Daisy Town</div>
         <div>
-          <div class="text-h4"><v-icon icon="mdi-office-building" class="mr-2"></v-icon>Hotel Selection</div>
+          <div class="text-h4">
+            <v-icon icon="mdi-office-building" class="mr-2"></v-icon>Hotel
+            Selection
+          </div>
           <HotelOverview />
         </div>
         <div v-for="section in sections" :key="section.title" class="my-6">
@@ -86,7 +89,22 @@ const sections = [
 </template>
 
 <style scoped>
+.v-main {
+  width: 100%;
+  height: 800px;
+  position: absolute;
+  background-image: linear-gradient(
+      rgba(255, 255, 255, 0),
+      rgba(255, 255, 255, 1)
+    ),
+    url("https://upload.wikimedia.org/wikipedia/commons/7/71/AntelopeHills.jpg");
+  background-size: 100%;
+  background-position-x: center;
+  background-position-y: bottom;
+}
+
 .content {
   max-width: 1000px;
+  margin-top: 300px;
 }
 </style>
