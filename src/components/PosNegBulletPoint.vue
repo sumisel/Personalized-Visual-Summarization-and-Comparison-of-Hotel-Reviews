4e57d286ee084fe56a26b9014b79ada2f39ab00b
Fabian Beck <fabian.beck@uni-bamberg.de>
2023-06-23 15:48:12 +0200

    Simplified PosNeg bullet points
    Moved details to tooltip

--- src/components/PosNegBulletPoint.vue
+++ src/components/PosNegBulletPoint.vue
@@ -1,8 +1,8 @@
 <script>
 import { useHotelStore } from "../stores/hotel.js";
-import { useClusterStore} from "../stores/cluster.js";
-import { useCategoryStore} from "../stores/category";
-import {globals} from "./../main";
+import { useClusterStore } from "../stores/cluster.js";
+import { useCategoryStore } from "../stores/category";
+import { globals } from "./../main";
 
 export default {
   props: {
@@ -26,126 +26,233 @@ export default {
     return { hotelStore, categoryStore, clusterStore, globals };
   },
   computed: {},
-  methods:{
+  methods: {
     calcFontSize: function (number) {
-      if (number < .25) {
-        return '8pt';
-      } else if (number < .5) {
-        return '11pt';
+      if (number < 0.25) {
+        return "8pt";
+      } else if (number < 0.5) {
+        return "11pt";
       } else {
-        return '14pt';
+        return "14pt";
       }
     },
     calcFontWeight: function (number) {
-      if (number < .25) {
-        return '100';
-      } else if (number < .5) {
-        return '300';
+      if (number < 0.25) {
+        return "100";
+      } else if (number < 0.5) {
+        return "300";
       } else {
-        return '500';
+        return "500";
       }
     },
     calcHover: function (hover, categoryId) {
       if (hover || this.clusterStore.noClusterHovered(categoryId)) {
         return 1;
       } else {
-        return .2;
+        return 0.2;
       }
     },
     highlight(categoryId, hotelId, num_items, polarity) {
-      console.log('mouseenter highlight');
-      this.emitter.emit("highlight_"+categoryId+'_'+hotelId.replaceAll('.', '_'), {categoryId, hotelId, num_items, polarity});
+      console.log("mouseenter highlight");
+      this.emitter.emit(
+        "highlight_" + categoryId + "_" + hotelId.replaceAll(".", "_"),
+        { categoryId, hotelId, num_items, polarity }
+      );
     },
     unhighlight(categoryId, hotelId) {
-      this.emitter.emit("unhighlight_"+categoryId+'_'+hotelId.replaceAll('.', '_'), {categoryId, hotelId});
+      this.emitter.emit(
+        "unhighlight_" + categoryId + "_" + hotelId.replaceAll(".", "_"),
+        { categoryId, hotelId }
+      );
     },
 
     matchText(text, word) {
-      return text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'').toLowerCase().split(' ')
-          .includes(word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'').toLowerCase().replace(/and|the|is|was|for/gi,''));
-    },
-    roundToInt: function (number) {
-      return number.toFixed(0);
+      return text
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+        .toLowerCase()
+        .split(" ")
+        .includes(
+          word
+            .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+            .toLowerCase()
+            .replace(/and|the|is|was|for/gi, "")
+        );
     },
     roundToDecimal: function (number, decimals) {
       return number.toFixed(decimals);
     },
   },
 };
-
 </script>
 
 <template>
-  <v-dialog class="d-flex justify-content-center"
-            scrollable
-            width="auto"
-            v-for="sentence in hotelStore.sentimentSummary(hotel, categoryId, polarity)"
-            :key="categoryId+'_'+polarity+'_'+hotel['id']+'_'+sentence['idx_summary']">
+  <v-dialog
+    class="d-flex justify-content-center"
+    scrollable
+    width="auto"
+    v-for="sentence in hotelStore.sentimentSummary(hotel, categoryId, polarity)"
+    :key="
+      categoryId +
+      '_' +
+      polarity +
+      '_' +
+      hotel['id'] +
+      '_' +
+      sentence['idx_summary']
+    "
+  >
     <template v-slot:activator="{ props }">
       <!--@mouseenter="clusterStore.hover(sentence['category'], sentence['cluster']); highlight(categoryId, hotel['id'], sentence['cluster_size'], polarity);"
         @mouseleave="clusterStore.unhover(sentence['category']); unhighlight(categoryId, hotel['id']);"
                   'opacity': calcHover(clusterStore.clustersById(sentence['category'])[sentence['cluster']]['hover'], sentence['category']),-->
-      <p v-bind="props"
-        @mouseenter=" highlight(categoryId, hotel['id'], sentence['cluster_size'], polarity);"
-        @mouseleave=" unhighlight(categoryId, hotel['id']);"
-        :style="[{'font-weight': calcFontWeight(sentence['ratio_category']),
-                  'font-size': calcFontSize(sentence['ratio_category']),
-                }]">
-        <v-icon v-if="polarity=='pos'" icon="mdi-plus-circle-outline" :style="[{'color': sentence['color']}]"/><v-icon v-else icon="mdi-minus-circle-outline" :style="[{'color': sentence['color']}]"/>
-        {{ sentence['text'] }}.
-        <br/>
-        {{ roundToDecimal(100*sentence['ratio_category'], 2) + '% of '+sentence['category']+' mentions'}}
-        <br/>
-        {{ roundToDecimal(100*sentence['cluster_size']/globals.$reviews[hotel['id']]['review_count'], 2) + '% of reviews'}}
-        <br/>
-        {{ roundToDecimal(sentence['centrality_score'], 2) + ' centrality score within cluster of similar reviews'}}
+      <p
+        v-bind="props"
+        @mouseenter="
+          highlight(categoryId, hotel['id'], sentence['cluster_size'], polarity)
+        "
+        @mouseleave="unhighlight(categoryId, hotel['id'])"
+        :style="[
+          {
+            'font-weight': calcFontWeight(sentence['ratio_category']),
+            'font-size': calcFontSize(sentence['ratio_category']),
+          },
+        ]"
+      >
+        <v-icon
+          v-if="polarity == 'pos'"
+          icon="mdi-plus-circle-outline"
+          :style="[{ color: sentence['color'] }]"
+        /><v-icon
+          v-else
+          icon="mdi-minus-circle-outline"
+          :style="[{ color: sentence['color'] }]"
+        />
+        {{ sentence["text"] }}.
+        <v-tooltip activator="parent" location="bottom" max-width="300px"
+          >
+          {{
+            roundToDecimal(100 * sentence["ratio_category"], 2) +
+            "% of " +
+            sentence["category"] +
+            " mentions"
+          }}
+          <br />
+          {{
+            roundToDecimal(
+              (100 * sentence["cluster_size"]) /
+                globals.$reviews[hotel["id"]]["review_count"],
+              2
+            ) + "% of reviews"
+          }}
+          <br />
+          {{
+            roundToDecimal(sentence["centrality_score"], 2) +
+            " centrality score within cluster of similar reviews"
+          }}</v-tooltip
+        >
       </p>
     </template>
     <template v-slot:default="{ isActive }">
-      <v-card style="width:30%;">
+      <v-card style="width: 30%">
         <v-toolbar>
-          <v-toolbar-title>{{ hotel['name'] +" - " + sentence['text'] }}</v-toolbar-title>
+          <v-toolbar-title>{{
+            hotel["name"] + " - " + sentence["text"]
+          }}</v-toolbar-title>
         </v-toolbar>
         <v-card-text>
-          <div class="text-h6">{{globals.$reviews[hotel['id']]['reviews'][sentence['idx_review']]['title']}}</div>
-          <div class="d-inline"
-               v-for="word in globals.$reviews[hotel['id']]['reviews'][sentence['idx_review']]['text'].split(' ')"
-               :style="[{'font-weight': calcFontWeight(matchText(sentence['text'], word)?1:0),
-                        'font-size': calcFontSize(matchText(sentence['text'], word)?1:.9),
-                        'color': matchText(sentence['text'], word)?sentence['color']:'black',
-                        }]">
-            {{word+' '}} </div>
+          <div class="text-h6">
+            {{
+              globals.$reviews[hotel["id"]]["reviews"][sentence["idx_review"]][
+                "title"
+              ]
+            }}
+          </div>
+          <span
+            v-for="(word, index) in globals.$reviews[hotel['id']]['reviews'][
+              sentence['idx_review']
+            ]['text'].split(' ')"
+            :style="[
+              {
+                'font-weight': calcFontWeight(
+                  matchText(sentence['text'], word) ? 1 : 0
+                ),
+                'font-size': calcFontSize(
+                  matchText(sentence['text'], word) ? 1 : 0.9
+                ),
+                color: matchText(sentence['text'], word)
+                  ? sentence['color']
+                  : 'black',
+              },
+            ]"
+            :key="index"
+          >
+            {{ word + " " }}
+          </span>
 
-          <br/>
+          <br />
           <v-divider></v-divider>
-          <br/>
+          <br />
 
           <div class="text-h5">Similar Review Points for this Hotel</div>
-          <br/>
-          <div v-for="review in sentence['idx_similar_reviews']">
+          <br />
+          <div v-for="review in sentence['idx_similar_reviews']" :key="review">
             <v-expansion-panels>
               <v-expansion-panel>
                 <v-expansion-panel-title>
-                  {{globals.$reviews[hotel['id']]['reviews'][review['idx_review']][polarity+'_aspects'][review['idx_sentence']]}}
+                  {{
+                    globals.$reviews[hotel["id"]]["reviews"][
+                      review["idx_review"]
+                    ][polarity + "_aspects"][review["idx_sentence"]]
+                  }}
                 </v-expansion-panel-title>
                 <v-expansion-panel-text>
-                  <div class="d-inline"
-                       v-for="word in globals.$reviews[hotel['id']]['reviews'][review['idx_review']]['text'].split(' ')"
-                       :style="[{'font-weight': calcFontWeight(matchText(globals.$reviews[hotel['id']]['reviews'][review['idx_review']][polarity+'_aspects'][review['idx_sentence']], word)?1:0),
-                        'font-size': calcFontSize(matchText(globals.$reviews[hotel['id']]['reviews'][review['idx_review']][polarity+'_aspects'][review['idx_sentence']], word)?1:.9),
-                        'color': matchText(globals.$reviews[hotel['id']]['reviews'][review['idx_review']][polarity+'_aspects'][review['idx_sentence']], word)?sentence['color']:'black',
-                        }]">
-                    {{word+' '}} </div>
+                  <span
+                    v-for="(word, index) in globals.$reviews[hotel['id']]['reviews'][
+                      review['idx_review']
+                    ]['text'].split(' ')"
+                    :style="[
+                      {
+                        'font-weight': calcFontWeight(
+                          matchText(
+                            globals.$reviews[hotel['id']]['reviews'][
+                              review['idx_review']
+                            ][polarity + '_aspects'][review['idx_sentence']],
+                            word
+                          )
+                            ? 1
+                            : 0
+                        ),
+                        'font-size': calcFontSize(
+                          matchText(
+                            globals.$reviews[hotel['id']]['reviews'][
+                              review['idx_review']
+                            ][polarity + '_aspects'][review['idx_sentence']],
+                            word
+                          )
+                            ? 1
+                            : 0.9
+                        ),
+                        color: matchText(
+                          globals.$reviews[hotel['id']]['reviews'][
+                            review['idx_review']
+                          ][polarity + '_aspects'][review['idx_sentence']],
+                          word
+                        )
+                          ? sentence['color']
+                          : 'black',
+                      },
+                    ]"
+                    :key="index"
+                  >
+                    {{ word + " " }}
+                  </span>
                 </v-expansion-panel-text>
               </v-expansion-panel>
             </v-expansion-panels>
           </div>
         </v-card-text>
         <v-card-actions class="justify-end">
-          <v-btn
-              variant="text"
-              @click="isActive.value = false"
-          >Close</v-btn>
+          <v-btn variant="text" @click="isActive.value = false">Close</v-btn>
         </v-card-actions>
       </v-card>
     </template>
