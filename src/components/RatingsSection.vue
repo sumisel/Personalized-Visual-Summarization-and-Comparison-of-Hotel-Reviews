a932f84e4167355ea1106b6f04cc26b1fbd51b54
Fabian Beck <fabian.beck@uni-bamberg.de>
2023-06-28 21:22:43 +0200

    Listing best rated hotels

--- src/components/RatingsSection.vue
+++ src/components/RatingsSection.vue
@@ -5,10 +5,12 @@ import { useCategoryStore } from "../stores/category.js";
 import { inject } from "vue";
 import Instruction from "./Instruction.vue";
 
+import InlineListItem from "./InlineListItem.vue";
 export default {
   components: {
     HotelRating,
     Instruction,
+    InlineListItem,
   },
   setup() {
     const hotelStore = useHotelStore();
@@ -40,6 +42,25 @@ export default {
       }
       return "with clear differences";
     },
+    topRatedHotels() {
+      if (this.hotelStore.selectedHotelIds.length === 0) {
+        return [];
+      }
+      // sort hotels by rating
+      const ratings = this.hotelStore.selectedHotelIds.map((hotelId) => {
+        return {
+          hotelId: hotelId,
+          rating: this.hotelStore.overallRating(hotelId),
+        };
+      });
+      ratings.sort((a, b) => b.rating - a.rating);
+      // cut off if difference to best hotel is larger than 0.2
+      const bestRating = ratings[0].rating;
+      const topRatedHotels = ratings.filter(
+        (ratedHotel) => bestRating - ratedHotel.rating < 0.2
+      );
+      return topRatedHotels.map((ratedHotel) => ratedHotel.hotelId);
+    },
     prioritiesUnchanged() {
       return this.categoryStore.categories.every(
         (category) => category.value === 50
@@ -55,12 +76,29 @@ export default {
   >
   <div>
     <p>
-      Customers have rated the
-      <b
-        ><span>{{ hotelStore.selectedHotelIds.length }}</span></b
-      >
-      selected hotels <b>{{ ratingVarietyDescription }}</b> according to the
-      categories weighted as prioritized.
+      <span>
+        Customers have rated the
+        <b
+          ><span>{{ hotelStore.selectedHotelIds.length }}</span></b
+        >
+        selected hotels <b>{{ ratingVarietyDescription }}</b> according to the
+        categories weighted as prioritized.
+      </span>
+      <span v-if="topRatedHotels.length > 0">
+        <InlineListItem
+          v-for="(hotelId, index) in topRatedHotels"
+          :key="hotelId"
+          :index="index"
+          :listLength="topRatedHotels.length"
+        >
+          <strong>{{ hotelMeta[hotelId].name }}</strong>
+        </InlineListItem>
+        <span v-if="topRatedHotels.length > 1"> are </span>
+        <span v-else> is </span> the overall best fitting hotel<span
+          v-if="topRatedHotels.length > 1"
+          >s</span
+        >.
+      </span>
     </p>
     <div class="d-flex flex-column hotel-list my-4">
       <HotelRating
