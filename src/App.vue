095742aa4111624583b7c5d23922dd91145fcf78
Fabian Beck <fabian.beck@uni-bamberg.de>
2023-06-28 22:48:38 +0200

    Removed caret from text animation
    So it does not look like you can type into the text field

--- src/App.vue
+++ src/App.vue
@@ -142,11 +142,10 @@ const scrollTo = (hash) => {
       // source: https://css-tricks.com/snippets/css/typewriter-effect/
       font-family: monospace;
       overflow: hidden; /* Ensures the content is not revealed until the animation */
-      border-right: 0.15em solid black; /* The typwriter cursor */
       white-space: nowrap; /* Keeps the content on a single line */
       margin: 0 auto; /* Gives that scrolling effect as the typing happens */
       letter-spacing: 0.17em; /* Adjust as needed */
-      animation: typing 8s steps(68, end), blink-caret 0.8s step-end infinite;
+      animation: typing 8s steps(68, end);
       font-size: 1.2rem;
     }
   }
@@ -158,17 +157,6 @@ const scrollTo = (hash) => {
     }
     to {
       width: 100%;
-    }
-  }
-
-  /* The typewriter cursor effect */
-  @keyframes blink-caret {
-    from,
-    to {
-      border-color: transparent;
-    }
-    50% {
-      border-color: black;
     }
   }
 }
