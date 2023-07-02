d2e2c4b17454d04b783e2b098416cc9c97f2fd48
Fabian Beck <fabian.beck@uni-bamberg.de>
2023-07-02 11:51:40 +0200

    Fix: underscore in POI name

--- src/components/Personalization.vue
+++ src/components/Personalization.vue
@@ -19,23 +19,14 @@ const poiMeta = inject("poiMeta");
       <div class="text-h6 mb-4">
         <v-icon>mdi-map-marker-radius</v-icon> Points of Interest
       </div>
-      <v-autocomplete
-        label="Choose your favorites"
-        :items="Object.keys(poiMeta)"
-        v-model="poiStore.selectedPois"
-        chips
-        closable-chips
-        multiple
-      >
+      <v-autocomplete label="Choose your favorites" :items="Object.keys(poiMeta)" v-model="poiStore.selectedPois" chips
+        closable-chips multiple>
         <template v-slot:chip="{ props, item }">
           <PoiChip :poi="item.raw" v-bind="props"></PoiChip>
         </template>
         <template v-slot:item="{ props, item }">
-          <v-list-item
-            v-bind="props"
-            :prepend-icon="poiMeta[item.raw].icon"
-            :title="item.raw"
-          ></v-list-item>
+          <v-list-item v-bind="props" :prepend-icon="poiMeta[item.raw].icon"
+            :title="item.raw.replaceAll('_', ' ')"></v-list-item>
         </template>
       </v-autocomplete>
     </div>
@@ -44,14 +35,12 @@ const poiMeta = inject("poiMeta");
       <div class="d-flex justify-center">
         <Glyph></Glyph>
         <div>
-          <v-icon icon="mdi-information-outline" size="small"></v-icon
-          ><v-tooltip activator="parent" location="bottom" max-width="300px"
-            >For every hotel, such a pie chart shows the ratings across all
+          <v-icon icon="mdi-information-outline" size="small"></v-icon><v-tooltip activator="parent" location="bottom"
+            max-width="300px">For every hotel, such a pie chart shows the ratings across all
             categories. For comparison, the bright area in the middle indicates
             the mininum rating of a category among the selected hotels. Pie
             slices are scaled according to priorities, as well as the overall
-            rating in the center is weighted by these priorities.</v-tooltip
-          >
+            rating in the center is weighted by these priorities.</v-tooltip>
         </div>
       </div>
       <v-table density="compact">
@@ -62,30 +51,14 @@ const poiMeta = inject("poiMeta");
           </tr>
         </thead>
         <tbody>
-          <tr
-            v-for="category in categories"
-            :key="category.id"
-            :class="{ hover: category.hover }"
-            @mouseenter="categoryStore.hover(category.id)"
-            @mouseleave="categoryStore.unhover()"
-          >
+          <tr v-for="category in categories" :key="category.id" :class="{ hover: category.hover }"
+            @mouseenter="categoryStore.hover(category.id)" @mouseleave="categoryStore.unhover()">
             <td>
-              <v-icon
-                :icon="category.icon"
-                class="mr-2"
-                :color="category.color"
-              ></v-icon
-              >{{ category.title }}
+              <v-icon :icon="category.icon" class="mr-2" :color="category.color"></v-icon>{{ category.title }}
             </td>
             <td>
-              <v-slider
-                :color="category.color"
-                v-model="category.value"
-                hide-details="true"
-                thumb-size="12"
-                track-size="2"
-                min="0.01"
-              ></v-slider>
+              <v-slider :color="category.color" v-model="category.value" hide-details="true" thumb-size="12"
+                track-size="2" min="0.01"></v-slider>
             </td>
           </tr>
         </tbody>
@@ -94,5 +67,4 @@ const poiMeta = inject("poiMeta");
   </v-sheet>
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
