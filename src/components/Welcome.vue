4a8795cd75e4b0556a999ba95177879c47824305
Fabian Beck <fabian.beck@uni-bamberg.de>
2023-07-11 16:31:47 +0200

    Extended welcome message

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,5 +1,6 @@
 <template>
-    <v-card>
+    <v-card class="elevation-6 pa-4">
+        <v-img src="/logo.png"></v-img>
         <v-card-title>
             Welcome to <strong>ViSCitR</strong>
         </v-card-title>
@@ -13,7 +14,11 @@
         <v-card-text>
             <p>The prototype demonstrates how a selection of hotels can be compared regarding customer ratings and reviews.
                 It is not meant to compete with interfaces of hotel booking platforms, but to complement them with a detail
-                comparison view, after available accommodations are filtered by availability, price range, and area.</p>
+                comparison view after available accommodations are filtered by availability, price range, and area. Also,
+                some information and features that you would expect when such a view is integrated in a booking platform are
+                not available, for instance, information of prices or features to freely explore the map.</p>
+            <p>Please note that, although we work with real review data of real hotels, we have obfuscated hotel names to
+                allow hotel comparison without biases of familiarity and previous experience.</p>
         </v-card-text>
         <v-card-actions>
             <strong class="ml-2 mr-8">Please select a city to start:</strong>
@@ -31,5 +36,14 @@
     left: 50%;
     transform: translate(-50%, -50%);
     width: 800px;
+
+    & .v-img {
+        height: 200px !important;
+        margin: 2rem auto;
+    }
+
+    & p {
+        margin: 0.5rem 0;
+    }
 }
 </style>
