31db854c5e0df1fabda9b501cca133677f4e9ad3
Franziska Huth <sumisel@gmail.com>
2023-07-25 19:42:12 +0200

    pos neg only show overall summary

--- src/components/PosNeg.vue
+++ src/components/PosNeg.vue
@@ -21,7 +21,8 @@ export default {
     const clusterStore = useClusterStore();
     const hotelMeta = inject("hotelMeta");
     const city = inject("city");
-    return { hotelStore, categoryStore, clusterStore, hotelMeta, city };
+    const reviews = inject("reviews");
+    return { hotelStore, categoryStore, clusterStore, hotelMeta, city, reviews };
   },
   computed: {},
   methods: {
@@ -37,6 +38,53 @@ export default {
       });
       return counts;
     },
+    sentimentSummary(hotelId, category, prefix) {
+      const reviews = inject("reviews");
+      let summary = [];
+      // compile sentences from all categories if it's the overall summary
+      if (category == "overall") {
+        for (let cat of this.categoryStore.relevantCategories) {
+          let num_sentences = 0;
+          if (cat["value"] > 33) {
+            num_sentences = 1;
+          }
+          if (cat["value"] > 66) {
+            num_sentences = 2;
+          }
+          let sentences = reviews[hotelId][prefix + "_summary"][cat["id"]]
+              .sort((a, b) => a["idx_summary"] - b["idx_summary"])
+              .slice(0, num_sentences);
+          sentences.forEach((sentence, i) => {
+            sentence["color"] = cat["color"];
+            sentence["category"] = cat["id"];
+          });
+          summary.push(...sentences);
+        }
+      } else {
+        summary = reviews[hotelId][prefix + "_summary"][category]
+            .sort((a, b) => a["idx_summary"] - b["idx_summary"])
+            .slice(0, 5);
+
+        summary.forEach((sentence, i) => {
+          sentence["color"] =
+              this.categoryStore.categoriesById[category]["color"];
+          sentence["category"] = category;
+        });
+      }
+      summary.forEach((sentence, i) => {
+        sentence["text"] =
+            reviews[hotelId]["reviews"][sentence["idx_review"]][
+            prefix + "_aspects"
+                ][sentence["idx_sentence"]];
+        sentence["idx_similar_reviews"].forEach((rev, j) => {
+          rev["text"] =
+              reviews[hotelId]["reviews"][rev["idx_review"]][prefix + "_aspects"][
+                  rev["idx_sentence"]
+                  ];
+        });
+      });
+      return summary;
+    },
   },
   data: () => ({
     panel: [0, 1],
@@ -47,101 +95,127 @@ export default {
 <template>
   <div v-if="hotelStore.selectedHotelIds.length > 1">
     <div class="my-2 flex-grow-1">
-      <v-expansion-panels v-model="panel">
-        <v-expansion-panel>
-          <v-expansion-panel-title :key="'title_overall'">
-            <v-row>
-              <div class="pa-2 hotel-name">
-                <b><u>Sentiment Overview</u></b>
-              </div>
-              <div class="pa-2 sentiment-text-title"></div>
-              <div class="pa-2">
-                <ChartPosNeg
-                  :categoryId="'overall'"
-                  :hotelId="'selected'"
-                  :posNeg="
-                    countsCategoryPosNeg('overall', hotelStore.selectedHotelIds)
-                  "
-                  :color="'#999999'"
-                  :width="200"
-                  :height="20"
-                  :xMin="-6"
-                  :xMax="6"
-                ></ChartPosNeg>
-              </div>
-              <div class="pa-2 sentiment-text-title"></div>
-            </v-row>
-          </v-expansion-panel-title>
-          <v-expansion-panel-text>
-            <v-table class="my-2 flex-grow-1">
-              <template
-                v-for="hotelId in hotelStore.selectedHotelIds"
-                :key="'overall_' + hotelId"
-              >
-                <tr>
-                  <td class="pa-2 hotel-name"></td>
-                  <td colspan="3" class="sentiment-chart">
-                    <ChartPosNeg
-                      :categoryId="'overall'"
-                      :hotelId="hotelId"
-                      :posNeg="countsCategoryPosNeg('overall', [hotelId])"
-                      :color="'#999999'"
-                      :width="200"
-                      :height="10"
-                      :xMin="-6"
-                      :xMax="6"
-                    ></ChartPosNeg>
+      <p>
+        <span>
+          This is a placeholder text for a short summary about the similarities and differences between in reviews of the different hotels.
+        </span>
+      </p>
+      <v-table class="my-2 flex-grow-1">
+        <v-card  class="my-2 flex-grow-1"
+          v-for="hotelId in hotelStore.selectedHotelIds"
+          :key="'overall_' + hotelId"
+        >
+          <tr>
+            <td class="pa-2 hotel-name"></td>
+            <td colspan="3" class="sentiment-chart">
+              <ChartPosNeg
+                :categoryId="'overall'"
+                :hotelId="hotelId"
+                :posNeg="countsCategoryPosNeg('overall', [hotelId])"
+                :color="'#999999'"
+                :width="200"
+                :height="10"
+                :xMin="-6"
+                :xMax="6"
+              ></ChartPosNeg>
+            </td>
+          </tr>
+          <tr>
+            <td class="pa-2 hotel-name">
+              <HotelName :hotelId="hotelId" avatar line-break></HotelName></td>
+            <td class="pa-2">
+              <v-table class="my-2 flex-grow-1">
+                <tr
+                    v-for="category in categoryStore.relevantCategories"
+                    :category="category"
+                    :key="'posneg_' + hotelId + '_' + category['id']"
+                >
+                  <td class="sentiment-text">
+                    <v-table>
+                      <tr
+                          v-for="sentence in sentimentSummary(hotelId, 'overall', 'neg')
+                      // filter to only show clusters with more than 1% of reviews
+                      .filter(
+                        (s) =>
+                          s['cluster_size'] > this.reviews[hotelId].review_count * 0.01
+                          &&
+                          s['category'] == category['id']
+                      )"
+                          :key="
+                          category['id'] +
+                          '_' +
+                          'neg' +
+                          '_' +
+                          hotelId +
+                          '_' +
+                          sentence['idx_summary']
+                        "
+                      >
+                        <td>
+                          <PosNegBulletPoint
+                              :hotelId="hotelId"
+                              :categoryId="category['id']"
+                              :polarity="'neg'"
+                              :sentence = "sentence"
+                              :key="'neg_' + hotelId + '_' + category['id']"
+                          >
+                          </PosNegBulletPoint>
+                        </td>
+                      </tr>
+                    </v-table>
                   </td>
-                </tr>
-                <tr>
-                  <td class="pa-2 hotel-name">
-                    <HotelName :hotelId="hotelId" avatar line-break></HotelName></td>
-                  <td class="pa-2 sentiment-text">
-                    <PosNegBulletPoint
-                      :hotelId="hotelId"
-                      :categoryId="'overall'"
-                      :polarity="'neg'"
-                      :key="'neg_overall_bullet_points'"
-                    >
-                    </PosNegBulletPoint>
-                  </td>
+
                   <td class="placeholder"></td>
-                  <td class="pa-2 sentiment-text">
-                    <PosNegBulletPoint
-                      :hotelId="hotelId"
-                      :categoryId="'overall'"
-                      :polarity="'pos'"
-                      :key="'pos_overall_bullet_points'"
-                    >
-                    </PosNegBulletPoint>
+
+                  <td class="sentiment-text">
+                    <v-table>
+                      <tr
+                          v-for="sentence in sentimentSummary(hotelId, 'overall', 'pos')
+                      // filter to only show clusters with more than 1% of reviews
+                      .filter(
+                        (s) =>
+                          s['cluster_size'] > this.reviews[hotelId].review_count * 0.01
+                          &&
+                          s['category'] == category['id']
+                      )"
+                          :key="
+                          category['id'] +
+                          '_' +
+                          'neg' +
+                          '_' +
+                          hotelId +
+                          '_' +
+                          sentence['idx_summary']
+                        "
+                      >
+                        <td>
+                          <PosNegBulletPoint
+                              :hotelId="hotelId"
+                              :categoryId="category['id']"
+                              :polarity="'pos'"
+                              :sentence = "sentence"
+                              :key="'neg_' + hotelId + '_' + category['id']"
+                          >
+                          </PosNegBulletPoint>
+                        </td>
+                      </tr>
+                    </v-table>
                   </td>
                 </tr>
-              </template>
-            </v-table>
-          </v-expansion-panel-text>
-        </v-expansion-panel>
-      </v-expansion-panels>
+              </v-table>
+
+            </td>
+          </tr>
+        </v-card>
+      </v-table>
     </div>
 
-    <div class="d-flex flex-column my-4">
-      <CategoryPosNeg
-        v-for="category in categoryStore.relevantCategories"
-        :category="category"
-        :key="'posneg_card_' + category['id']"
-        @mouseenter="categoryStore.hover(category['id'])"
-        @mouseleave="categoryStore.unhover()"
-      ></CategoryPosNeg>
-    </div>
   </div>
 </template>
 
 <style>
 .hotel-name {
   width: 20% !important;
-}
-
-.sentiment-text-title {
-  width: 29.7% !important;
 }
 
 .sentiment-text {
@@ -156,6 +230,5 @@ export default {
   width: 100% !important;
   text-align: center;
   vertical-align: middle;
-  border-top: 1px solid grey;
 }
 </style>
--- src/components/PosNegBulletPoint.vue
+++ src/components/PosNegBulletPoint.vue
@@ -9,6 +9,7 @@ export default {
     hotelId: String,
     categoryId: String,
     polarity: String,
+    sentence: Object,
   },
   setup() {
     const hotelStore = useHotelStore();
@@ -80,53 +81,6 @@ export default {
     roundToDecimal: function (number, decimals) {
       return number.toFixed(decimals);
     },
-    sentimentSummary(hotelId, category, prefix) {
-      const reviews = inject("reviews");
-      let summary = [];
-      // compile sentences from all categories if it's the overall summary
-      if (category == "overall") {
-        for (let cat of this.categoryStore.relevantCategories) {
-          let num_sentences = 0;
-          if (cat["value"] > 33) {
-            num_sentences = 1;
-          }
-          if (cat["value"] > 66) {
-            num_sentences = 2;
-          }
-          let sentences = reviews[hotelId][prefix + "_summary"][cat["id"]]
-            .sort((a, b) => a["idx_summary"] - b["idx_summary"])
-            .slice(0, num_sentences);
-          sentences.forEach((sentence, i) => {
-            sentence["color"] = cat["color"];
-            sentence["category"] = cat["id"];
-          });
-          summary.push(...sentences);
-        }
-      } else {
-        summary = reviews[hotelId][prefix + "_summary"][category]
-          .sort((a, b) => a["idx_summary"] - b["idx_summary"])
-          .slice(0, 5);
-
-        summary.forEach((sentence, i) => {
-          sentence["color"] =
-            this.categoryStore.categoriesById[category]["color"];
-          sentence["category"] = category;
-        });
-      }
-      summary.forEach((sentence, i) => {
-        sentence["text"] =
-          reviews[hotelId]["reviews"][sentence["idx_review"]][
-            prefix + "_aspects"
-          ][sentence["idx_sentence"]];
-        sentence["idx_similar_reviews"].forEach((rev, j) => {
-          rev["text"] =
-            reviews[hotelId]["reviews"][rev["idx_review"]][prefix + "_aspects"][
-              rev["idx_sentence"]
-            ];
-        });
-      });
-      return summary;
-    },
   },
 };
 </script>
@@ -136,21 +90,6 @@ export default {
     class="d-flex justify-content-center"
     scrollable
     width="auto"
-    v-for="sentence in sentimentSummary(hotelId, categoryId, polarity)
-      // filter to only show clusters with more than 1% of reviews
-      .filter(
-        (sentence) =>
-          sentence['cluster_size'] > this.reviews[hotelId].review_count * 0.01
-      )"
-    :key="
-      categoryId +
-      '_' +
-      polarity +
-      '_' +
-      hotelId +
-      '_' +
-      sentence['idx_summary']
-    "
   >
     <template v-slot:activator="{ props }">
       <p
