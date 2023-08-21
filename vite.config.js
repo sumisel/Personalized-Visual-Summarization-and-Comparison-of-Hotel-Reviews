bbd6183e2a65f5d781b9d2d4eb139982cdf4dd04
Franziska Huth <sumisel@gmail.com>
2023-08-21 09:34:11 +0200

    added some config entries for the interface to run on my bwcloud server

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  esbuild: {
+    supported: {
+      'top-level-await': true //browsers can handle top-level-await features
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
