a1161cdcdaee8bd7ac55ba771ede8df9a6bbd60b
Franziska Huth <sumisel@gmail.com>
2024-09-09 18:15:19 +0200

    github pages config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === "production" ? "/Personalized-Visual-Summarization-and-Comparison-of-Hotel-Reviews/" : "/",
     css: {
       loaderOptions: {
         sass: {
